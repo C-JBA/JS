b019712715cfd1a43476c1096c2ed1bad825ed7d
CJBA <barajasavilacristobaljair@gmail.com>
2023-03-27 20:06:57 -0600

    Se agregan más funciones del ejercicio

---
+++ JS-04 - Arrays and Loops/Ejercicio 2/js/main.js
@@ -0,0 +1,9 @@
+
+
+    let strr="";
+    for(let j=1;j<6;j++){
+        strr+="* ";
+        console.log(strr);
+        
+    }
+
---
+++ JS-04 - Arrays and Loops/Ejercicio 3/js/main.js
@@ -0,0 +1,40 @@
+
+function Reduce_5(xValue){
+
+    while(xValue>0){
+console.log(xValue)
+xValue-=.5;
+    }
+};
+
+let ind=1;
+while(ind<=100){
+if(ind%2==0){
+}else{
+    console.log(ind);
+}
+ind++;
+}
+
+
+let inr=0;
+let ene=20;
+while(inr<=ene){
+console.log("["+inr+"]");
+inr++;
+}
+
+
+
+
+function sumaNumeros(hastaN){
+
+let ink=0;
+let summ=0;
+while(ink<=Number(hastaN)){
+summ+=ink;
+ink++;
+
+}
+return(summ);
+}
